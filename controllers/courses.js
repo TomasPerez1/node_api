e759648a8997bb033f4a2e18bf3533cb7304fcc6
tomasPerez <tomasperez.henry@gmail.com>
2025-06-25 20:02:35 -0300

    connect db

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,7 +1,11 @@
+const db = require("../db")
 
-function handleCursos(req, res) {
+async function handleCursos(req, res) {
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
+    const query = 'SELECT * FROM users';
+    const result = await db.query(query);
+    console.log(result);
     res.end(JSON.stringify({ message: "GET /cursos OK" }));
   } else {
     res.writeHead(405, { "Content-Type": "application/json" });
